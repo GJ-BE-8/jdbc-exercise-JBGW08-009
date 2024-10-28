af4d7711a7cdfed7398e15075f485717fc5c4e67
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2024-10-28 10:31:18 +0900

    commit

---
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StatementStudentRepository.java
@@ -0,0 +1,103 @@
+package com.nhnacademy.jdbc.student.repository.impl;
+
+import com.nhnacademy.jdbc.student.domain.Student;
+import com.nhnacademy.jdbc.student.repository.StudentRepository;
+import com.nhnacademy.jdbc.util.DbUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+
+@Slf4j
+public class StatementStudentRepository implements StudentRepository {
+
+    @Override
+    public int save(Student student){
+        //todo#1 insert student
+        String sql = String.format(
+                "insert into jdbc_students(id, name, gender, age) values ('%s', '%s', '%s', '%d')",
+                student.getId(),
+                student.getName(),
+                student.getGender(),
+                student.getAge()
+        );
+
+        try (Connection connection = DbUtils.getConnection();
+                Statement statement = connection.createStatement();)
+        {
+            int result = statement.executeUpdate(sql);
+            log.debug("save result {}", result);
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Optional<Student> findById(String id){
+        //todo#2 student 조회
+        String sql = String.format("select * from jdbc_students where id='%s'", id);
+
+        try (Connection connection = DbUtils.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql);
+        ) {
+            if (resultSet.next()) {
+                Student student = new Student(resultSet.getString("id"),
+                        resultSet.getString("name"),
+                        Student.GENDER.valueOf(resultSet.getString("gender")),
+                        resultSet.getInt("age"),
+                        resultSet.getTimestamp("created_at").toLocalDateTime());
+                return Optional.of(student);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public int update(Student student){
+        //todo#3 student 수정, name <- 수정합니다.
+        String sql = String.format("update jdbc_students set name='%s', gender='%s', age=%d where id='%s' ",
+                student.getName(),
+                student.getGender(),
+                student.getAge(),
+                student.getId()
+        );
+
+        try(Connection connection = DbUtils.getConnection();
+            Statement statement = connection.createStatement();
+        ) {
+            int result = statement.executeUpdate(sql);
+            log.debug("update result:{}",result);
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int deleteById(String id){
+        //todo#4 student 삭제
+        String sql = String.format("delete from jdbc_students where id = '%s'", id);
+//        if (findById(id).isPresent()) {
+            try(Connection connection = DbUtils.getConnection();
+                Statement statement = connection.createStatement();
+            ) {
+                int result = statement.executeUpdate(sql);
+                log.debug("delete reulst : {}", result);
+                return result;
+            } catch (SQLException e) {
+                throw new RuntimeException();
+            }
+//        } else {
+//            throw new RuntimeException();
+//        }
+    }
+
+}
