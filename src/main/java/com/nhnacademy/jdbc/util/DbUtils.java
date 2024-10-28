6cc30565ba55f354031695d94f7030b568673d8d
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2024-10-28 11:28:30 +0900

    commit

---
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -0,0 +1,57 @@
+package com.nhnacademy.jdbc.util;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.Duration;
+
+public class DbUtils {
+    public DbUtils(){
+        throw new IllegalStateException("Utility class");
+    }
+
+//    public static Connection getConnection() {
+//        Connection connection = null;
+//        try {
+//            //todo connection.
+//            connection = DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_0","nhn_academy_0","1j0OTaJgkS1@$dWl");
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        return connection;
+//    }
+
+    private static final DataSource DATASOURCE;
+
+    static {
+        BasicDataSource basicDataSource = new BasicDataSource();
+
+        final String url = "jdbc:mysql://133.186.241.167:3306/nhn_academy_9";;
+        final String name = "nhn_academy_9";
+        final String password = "iRz&E!F&XxG4d?kP";
+
+        //todo#0 {ip},{database},{username},{password}
+        basicDataSource.setUrl(url);
+        basicDataSource.setUsername(name);
+        basicDataSource.setPassword(password);
+
+        basicDataSource.setInitialSize(5);
+        basicDataSource.setMaxTotal(5);
+        basicDataSource.setMaxIdle(5);
+        basicDataSource.setMinIdle(5);
+
+        basicDataSource.setMaxWait(Duration.ofSeconds(2));
+        basicDataSource.setValidationQuery("select 1");
+        basicDataSource.setTestOnBorrow(true);
+        DATASOURCE = basicDataSource;
+    }
+
+    public static DataSource getDataSource(){
+        return DATASOURCE;
+    }
+
+}
